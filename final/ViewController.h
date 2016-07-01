a41909b779090c5318b7007e25bd9171a645240f
ilabafrica <ilabafrica@saf04.local>
2016-07-01 12:26:18 +0300

    first commit

--- final/ViewController.h
+++ final/ViewController.h
@@ -5,9 +5,30 @@
 //  Created by ilabafrica on 7/1/16.
 //  Copyright (c) 2016 iLabAfrica. All rights reserved.
 //
+//
+//
 
-#import <UIKit/UIKit.h>
+#import<UIKit/UIKit.h>
+typedef enum{ Plus,Minus,Multiply,Divide} CalcOperation;
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    IBOutlet UITextField *display;
+    IBOutlet UIButton *cbutton;
+    NSString *storage;
+    CalcOperation operation;
+}
+
+- (IBAction) button1;
+- (IBAction) button2;
+- (IBAction) button3;
+- (IBAction) button4;
+- (IBAction) button5;
+- (IBAction) button6;
+- (IBAction) button7;
+- (IBAction) button9;
+- (IBAction) button0;
+- (IBAction) plusbutton;
+- (IBAction) equalsbutton;
+- (IBAction) clearDisplay;
 
 @end
--- final/ViewController.m
+++ final/ViewController.m
@@ -3,7 +3,7 @@
 //  final
 //
 //  Created by ilabafrica on 7/1/16.
-//  Copyright (c) 2016 iLabAfrica. All rights reserved.
+//  Copyright (c) 2016 STANiLabAfrica. All rights reserved.
 //
 
 #import "ViewController.h"
@@ -13,17 +13,104 @@
 @end
 
 @implementation ViewController
+- (IBAction) clearDisplay {
+    display.text = @"";
+}
 
+- (IBAction) button1 {
+    display.text=[NSString stringWithFormat:@"%@1",display.text];
+}
+- (IBAction) button2 {
+    display.text=[NSString stringWithFormat:@"%@2",display.text];
+}
+- (IBAction) button3 {
+    display.text=[NSString stringWithFormat:@"%@3",display.text];
+}
+
+- (IBAction) button4 {
+    display.text=[NSString stringWithFormat:@"%@4",display.text];
+}
+
+- (IBAction) button5 {
+    display.text=[NSString stringWithFormat:@"%@5",display.text];
+}
+
+- (IBAction) button6 {
+    display.text=[NSString stringWithFormat:@"%@6",display.text];
+}
+
+- (IBAction) button7 {
+    display.text=[NSString stringWithFormat:@"%@7",display.text];
+}
+
+- (IBAction) button8 {
+    display.text=[NSString stringWithFormat:@"%@8",display.text];
+}
+
+- (IBAction) button9 {
+    display.text=[NSString stringWithFormat:@"%@9",display.text];
+}
+
+- (IBAction) button0 {
+    display.text=[NSString stringWithFormat:@"%@0",display.text];
+}
+
+- (IBAction) plusbutton {
+    operation = Plus;
+    storage = display.text;
+    display.text=@"";
+}
+
+- (IBAction) minusbutton {
+    operation = Minus;
+    storage = display.text;
+    display.text=@"";
+}
+
+- (IBAction) multiplybutton {
+    operation = Multiply;
+    storage = display.text;
+    display.text=@"";
+}
+
+- (IBAction) dividebutton {
+    operation = Divide;
+    storage = display.text;
+    display.text=@"";
+}
+
+- (IBAction) equalsbutton {
+    NSString *val = display.text;
+    switch(operation) {
+        case Plus :
+            display.text= [NSString stringWithFormat:@"%qi",[val longLongValue]+[storage longLongValue]];
+            break;
+        case Minus:
+            display.text= [NSString stringWithFormat:@"%qi",[storage longLongValue]-[val longLongValue]];
+            break;
+        case Divide:
+            display.text= [NSString stringWithFormat:@"%qi",[storage longLongValue]/[val longLongValue]];
+            break;
+        case Multiply:
+            display.text= [NSString stringWithFormat:@"%qi",[val longLongValue]*[storage longLongValue]];
+            break;
+    }
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
+- (void)viewDidUnload
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
 @end
